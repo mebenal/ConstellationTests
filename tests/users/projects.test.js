beaddcb0411767dfa97a7eacc521aa72819238b3
PC <PC@BARBARA-11>
2020-07-15 10:57:54 -0600

    Updates

---
+++ tests/users/projects.test.js
@@ -0,0 +1,22 @@
+var rp = require('request-promise-native');
+
+it('Testing to see if Dev is up', async () =>{
+    
+    var options = {
+        uri: 'http://dev-constellation.citwdd.net/Projects',
+        headers:{
+        },
+    };
+
+    var errorWasCaught=false;
+    var errorCaught=null;
+
+    try{
+        var response = await rp(options);
+    } catch (exception){
+        errorCaught=exception;
+        errorWasCaught=true;
+    }
+    expect(errorWasCaught).toBe(false);//assertion of what is expected
+})
+
